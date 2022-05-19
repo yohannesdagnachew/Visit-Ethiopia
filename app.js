6b2f45bd3fbd97078de15e61032d918ea8c5c45f
yohannesdagnachew <102184682+yohannesdagnachew@users.noreply.github.com>
2022-05-19 11:02:00 +0300

    Replace double cot by single coton the js file

--- app.js
+++ app.js
@@ -1,57 +1,57 @@
 let speakersArry = [
   {
-    photo: "images/macron.jpeg",
-    name: "Macron",
-    job: "France Prisedant",
+    photo: 'images/macron.jpeg',
+    name: 'Macron',
+    job: 'France Prisedant',
     says: `One of Ethiopia’s national wonders is its famous rock-hewn churches of Lalibela.`,
   },
   {
-    photo: "images/bob.jpeg",
-    name: "BOB",
-    job: "Musician",
+    photo: 'images/bob.jpeg',
+    name: 'BOB',
+    job: 'Musician',
     says: `One of Ethiopia’s national wonders is its famous rock-hewn churches of Lalibela.`,
   },
   {
-    photo: "images/mandela.jpeg",
-    name: "Mndela",
-    job: "S.A Prisedant",
+    photo: 'images/mandela.jpeg',
+    name: 'Mndela',
+    job: 'S.A Prisedant',
     says: `One of Ethiopia’s national wonders is its famous rock-hewn churches of Lalibela.`,
   },
   {
-    photo: "images/sayat.jpeg",
-    name: "Sayat",
-    job: "Model",
+    photo: 'images/sayat.jpeg',
+    name: 'Sayat',
+    job: 'Model',
     says: `One of Ethiopia’s national wonders is its famous rock-hewn churches of Lalibela.`,
   },
   {
-    photo: "images/rita.jpeg",
-    name: "Rita",
-    job: "Musician",
+    photo: 'images/rita.jpeg',
+    name: 'Rita',
+    job: 'Musician',
     says: `One of Ethiopia’s national wonders is its famous rock-hewn churches of Lalibela.`,
   },
   {
-    photo: "images/nati.jpeg",
-    name: "Nati",
-    job: "Musician",
+    photo: 'images/nati.jpeg',
+    name: 'Nati',
+    job: 'Musician',
     says: `One of Ethiopia’s national wonders is its famous rock-hewn churches of Lalibela.`,
   },
 ];
-const speakersContainer = document.querySelector(".speakers-container");
+const speakersContainer = document.querySelector('.speakers-container');
 
 const addSpeaker = () => {
   for (let i = 0; i < speakersArry.length; i += 1) {
-    const speaker = document.createElement("div");
-    speaker.innerHTML = `<div  class="speakers-container-item"><img class="speaker-image" src="${speakersArry[i].photo}" alt="">
-    <div class="separetor">
+    const speaker = document.createElement('div');
+    speaker.innerHTML = `<div  class='speakers-container-item'><img class='speaker-image' src='${speakersArry[i].photo}' alt=''>
+    <div class='separetor'>
         <p >${speakersArry[i].name}</p>
-        <p class="speaker-job">${speakersArry[i].job}</p>
-        <p class="speaker-says">${speakersArry[i].says}</p>
+        <p class='speaker-job'>${speakersArry[i].job}</p>
+        <p class='speaker-says'>${speakersArry[i].says}</p>
     </div>`;
     speakersContainer.appendChild(speaker);
     if (i >= 2) {
-      speaker.classList.add("more");
+      speaker.classList.add('more');
     }
   }
 };
 
-window.addEventListener("load", addSpeaker);
+window.addEventListener('load', addSpeaker);
